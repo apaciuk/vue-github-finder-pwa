e6e3eba4e59a07d1ff420939d45d0cb61eea2be5
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-22 23:41:43 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -4,12 +4,7 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import './index.js'
-import axios from "axios";
 import "./registerServiceWorker";
-
-Vue.prototype.$http = axios;
-Vue.prototype.axios = axios;
-
 
 Vue.config.productionTip = false
 
